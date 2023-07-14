aaa777c933cfd2570c5c16eb848346d486b7b429
hsu1261 <xuzing105525@gmail.com>
2023-07-15 01:57:10 +0800

    Fixed mask bug
    zooming offset problem still exists

--- spectrum_observer.py
+++ spectrum_observer.py
@@ -42,17 +42,20 @@ def longest_slice_between_duplicate_elements(lst):
 
 
 class ZoomDrag(tk.Canvas):
-    def __init__(self, master: any, size: int = 400, bg = 'black', **kwargs):
-        super().__init__(master, width=size, height=size, bg= bg, **kwargs)
-        self.canv_len = size
+    def __init__(self, master: any, width:int= 1000, height:int= 600, bg = 'black', **kwargs):
+        super().__init__(master, width=width, height=height, bg= bg, **kwargs)
         self.scale_factor:int  = 1
         self.offset = (0,0)
+        self.arr = None
         self.image_id = None
+        self.mask_id = None
         self.drawing = False
         self.lines = []
+        self.mask_color = (255,0,0,40)
 
-        self.bind('<Button-1>', self.start_drag)
+        self.bind('<ButtonPress-1>', self.start_drag)
         self.bind('<B1-Motion>', self.drag)
+        self.bind('<ButtonRelease-1>', self.stop_drag)
         self.bind('<MouseWheel>', self.zoom)
         self.bind('<Button-4>', self.zoom)
         self.bind('<Button-5>', self.zoom)
@@ -61,11 +64,12 @@ class ZoomDrag(tk.Canvas):
         self.bind("<B3-Motion>", self.draw)
 
     def load_array(self, arr):
-
         self.delete('all')
+        self.arr = arr
         self.scale_factor:int  = 1
         self.offset = (0,0)
         self.image_id = None
+        self.mask_id = None
         if arr is not None:
             onechan = arr[:,:,49]
             normed_gray = (((onechan - np.min(onechan)) / (np.max(onechan) - np.min(onechan))) * 255).astype('uint8')
@@ -73,23 +77,55 @@ class ZoomDrag(tk.Canvas):
             self.pil_image = normed_gray
             self.update_image()
 
+            self.addtag("image", 'withtag', self.image_id)
+
     def start_drag(self, event):
         self.start_x, self.start_y = event.x, event.y
     
     def drag(self, event):
-        if self.drawing:
+        if self.drawing or not self.image_id:
             return
         dx = event.x- self.start_x
         dy = event.y- self.start_y
-        self.move(self.image_id, dx, dy)
+        self.move('image', dx, dy)
+
+        # constrain movement
+        bbox = self.bbox(self.image_id)
+        if (bbox[2]-bbox[0])<= self.winfo_width():
+            if bbox[0] < 0:
+                self.move('image', -bbox[0], 0)
+            if bbox[2] > self.winfo_width():
+                self.move('image', self.winfo_width() - bbox[2], 0)
+        else:
+            if bbox[0] > 0:
+                self.move('image', -bbox[0], 0)
+            if bbox[2] < self.winfo_width():
+                self.move('image', self.winfo_width() - bbox[2], 0)
+
+        if (bbox[3]-bbox[1])<= self.winfo_height():
+            if bbox[1] < 0:
+                self.move('image', 0, -bbox[1])
+            if bbox[3] > self.winfo_height():
+                self.move('image', 0, self.winfo_height() - bbox[3])
+        else:
+            if bbox[1] > 0:
+                self.move('image', 0, -bbox[1])
+            if bbox[3] < self.winfo_height():
+                self.move('image', 0, self.winfo_height() - bbox[3])
+        # self.configure(scrollregion=self.bbox(self.image_id))     # makes the item in canvas unable to exceed border of canvas
+                                                                    # but occers error in coordinate when drag over border
 
         self.start_x, self.start_y = event.x, event.y
 
-        # update offset
-        offset_x = self.offset[0]+ dx
-        offset_y = self.offset[1]+ dy
+    def stop_drag(self, event):
+        if self.drawing or not self.image_id:
+            return
+
+        # update offset based on true position
+        bbox = self.bbox(self.image_id)
+        offset_x = (bbox[0]+bbox[2]- self.winfo_width())// 2
+        offset_y = (bbox[1]+bbox[3]- self.winfo_height())// 2
         self.offset = offset_x, offset_y
-        self.configure(scrollregion=self.bbox("all"))
 
     def update_image(self):
         width = int(self.pil_image.width * self.scale_factor)
@@ -98,15 +134,15 @@ class ZoomDrag(tk.Canvas):
         self.resized_image = self.pil_image.resize((width, height), Image.Resampling.NEAREST)    # used nearest to avoid interpolation
         self.photo_image = ImageTk.PhotoImage(self.resized_image)
 
-        if self.image_id:
-            self.delete(self.image_id)
-
-        anchor_x, anchor_y = int(self.canv_len/2)+self.offset[0], int(self.canv_len/2)+self.offset[1]
-        self.image_id = self.create_image(anchor_x, anchor_y, image=self.photo_image, anchor='center')
-        self.configure(scrollregion=self.bbox("all"))           # makes the item in canvas unable to exceed border of canvas
+        anchor_x, anchor_y = int(self.winfo_width()/2)+self.offset[0], int(self.winfo_height()/2)+self.offset[1]
+        self.image_id = self.create_image(anchor_x, anchor_y, image=self.photo_image, anchor='center', tags= ('image'))
+        if self.mask_id:
+            self.tkmask = ImageTk.PhotoImage(self.mask.resize((self.mask.width*self.scale_factor,self.mask.height*self.scale_factor),
+                                                              Image.Resampling.NEAREST))
+            self.mask_id = self.create_image(anchor_x, anchor_y, image=self.tkmask, anchor= 'center', tags= ('image'))
 
     def zoom(self, event):
-        if self.drawing:
+        if self.drawing or not self.image_id:
             return
 
         if event.delta > 0 or event.num == 4:
@@ -118,7 +154,11 @@ class ZoomDrag(tk.Canvas):
 
     def start_drawing(self, event):
         global draw_start_x, draw_start_y
+        if not self.image_id:
+            return
         self.drawing = True
+        if self.mask_id:
+            self.delete(self.mask_id)
         draw_start_x, draw_start_y = event.x, event.y
 
     def stop_drawing(self, event):
@@ -138,9 +178,10 @@ class ZoomDrag(tk.Canvas):
             draw_start_x, draw_start_y = event.x, event.y
 
     def fill_closed_shape(self):
+        '''creates an alpha mask and show in canvas'''
         if len(self.lines)<2:
             return
-        
+
         line_coords = []
         for line in self.lines:
             coords = self.coords(line)
@@ -151,20 +192,28 @@ class ZoomDrag(tk.Canvas):
         if not largest_slice:
             largest_slice = line_coords
 
-        # Apply convex hull algorithm
+        # apply convex hull algorithm
         hull = ConvexHull(largest_slice)
         convex_coords = [largest_slice[i] for i in hull.vertices]
 
-        image = Image.new("RGBA", (self.winfo_width(), self.winfo_height()), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(image)
-        draw.polygon(convex_coords, fill=(128, 0, 0, 128))
+        # crop coordinates if exceed bbox of image
+        bbox = self.bbox(self.image_id)
+        canvas_coords = [(max(bbox[0], min(x, bbox[2])), max(bbox[1], min(y, bbox[3]))) for x, y in convex_coords]
 
-        self.drawed = ImageTk.PhotoImage(image)
-        anchor_x, anchor_y = int(self.canv_len/2)+self.offset[0], int(self.canv_len/2)+self.offset[1]
-        self.create_image(anchor_x, anchor_y, image=self.drawed, anchor='center')
+        # translate coordinates with reference to image item
+        item_coords = [(x-bbox[0], y-bbox[1]) for x, y in canvas_coords]
 
-        # self.resized_image = draw
-        # self.update_image()
+        self.mask = Image.new("RGBA", (bbox[2]-bbox[0], bbox[3]-bbox[1]), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(self.mask)
+        draw.polygon(item_coords, fill= self.mask_color)
+
+        if self.scale_factor != 1:
+            self.mask = self.mask.resize((self.pil_image.width, self.pil_image.height), Image.Resampling.NEAREST)
+            self.tkmask = ImageTk.PhotoImage(self.mask.resize((self.mask.width*self.scale_factor,self.mask.height*self.scale_factor),
+                                                              Image.Resampling.NEAREST))
+        else:
+            self.tkmask = ImageTk.PhotoImage(self.mask)
+        self.mask_id = self.create_image(bbox[0], bbox[1], image=self.tkmask, anchor=tk.NW, tags= ('image'))
 
 canvas = ZoomDrag(window)
 open_button = tk.Button(window, text="Open Image", command= open_hsi)
