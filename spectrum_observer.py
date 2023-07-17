512b96cf8a2fde2abc5ec0d355856503c0e9f0bf
hsu1261 <xuzing105525@gmail.com>
2023-07-17 23:04:27 +0800

    Main window finished
    prepare to add graph

--- spectrum_observer.py
+++ spectrum_observer.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox, colorchooser
 from spectral.io import envi
 import spectral
 from PIL import ImageTk, Image, ImageDraw
@@ -8,16 +8,23 @@ import os
 from scipy.spatial import ConvexHull
 
 spectral.settings.envi_support_nonlowercase_params = 'TRUE'
+os.chdir(r'D:\code\AI_with_hsi\data')
 
 # Create the main Tkinter window
-window = tk.Tk()
-window.title("Image Selection and Pixel Extraction")
-window.resizable(0,0)
+root = tk.Tk()
+root.title("Image Selection and Pixel Extraction")
+root.resizable(0,0)
+
+def choose_cwd():
+    '''Let user select directory where they import data'''
+
+    _ = filedialog.askdirectory(initialdir= os.getcwd())
+    if _:
+        os.chdir(_)
 
 def open_hsi():
     global image_path, hsi
-    image_path = filedialog.askopenfilename(filetypes=[("Array",'*.npy;*.hdr')],
-                                            initialdir= r'D:\code\AI_with_hsi\data\slices')
+    image_path = filedialog.askopenfilename(filetypes=[("Array",'*.npy;*.hdr')],initialdir= os.getcwd())
     if image_path:
         extention_type = os.path.splitext(image_path)[1]
         if extention_type == '.npy':
@@ -40,18 +47,45 @@ def longest_slice_between_duplicate_elements(lst):
         seen[element] = i       # Store the element and its index
     return longest_slice        # return empty array if no same element found
 
+def save_image():
+
+    name = os.path.basename(image_path).split('.')[0]
+
+    if os.path.exists(name+".png"):
+        response = messagebox.askquestion("File Exists", "A file with the same name already exists. Do you want to overwrite?")
+        if response == 'no':
+            return
+
+    image:Image.Image = canvas.resized_image
+    bbox = canvas.bbox(canvas.image_id)
+    x1 = max(-bbox[0], 0)
+    y1 = max(-bbox[1], 0)
+    x2 = x1+ min(image.width, canvas.winfo_width())
+    y2 = y1+ min(image.height, canvas.winfo_height())
+    image = image.crop((x1,y1,x2,y2))
+
+    if canvas.mask_id:
+        mask:Image.Image = canvas.resized_mask
+        mask = mask.crop((x1,y1,x2,y2))
+        image = image.convert('RGBA')
+        image = Image.alpha_composite(image, mask)
+
+    image.save(name+".png")
+    print("Image saved successfully.")
+
 
 class ZoomDrag(tk.Canvas):
-    def __init__(self, master: any, width:int= 1000, height:int= 600, bg = 'black', **kwargs):
+    def __init__(self, master: any, width:int= 1000, height:int= 750, bg = 'black', **kwargs):
         super().__init__(master, width=width, height=height, bg= bg, **kwargs)
-        self.scale_factor:int  = 1
-        self.offset = (0,0)
+        self.scale_factor = tk.IntVar(value= 1)
+        self.effective_offset = (0,0)
         self.arr = None
         self.image_id = None
         self.mask_id = None
         self.drawing = False
         self.lines = []
         self.mask_color = (255,0,0,40)
+        self.graph_color:str = '#ff0000'
 
         self.bind('<ButtonPress-1>', self.start_drag)
         self.bind('<B1-Motion>', self.drag)
@@ -62,12 +96,13 @@ class ZoomDrag(tk.Canvas):
         self.bind("<ButtonPress-3>", self.start_drawing)
         self.bind("<ButtonRelease-3>", self.stop_drawing)
         self.bind("<B3-Motion>", self.draw)
+        self.bind('<Button-2>', self.pick_color)
 
     def load_array(self, arr):
         self.delete('all')
         self.arr = arr
-        self.scale_factor:int  = 1
-        self.offset = (0,0)
+        self.scale_factor.set(1)
+        self.effective_offset = (0,0)
         self.image_id = None
         self.mask_id = None
         if arr is not None:
@@ -125,20 +160,24 @@ class ZoomDrag(tk.Canvas):
         bbox = self.bbox(self.image_id)
         offset_x = (bbox[0]+bbox[2]- self.winfo_width())// 2
         offset_y = (bbox[1]+bbox[3]- self.winfo_height())// 2
-        self.offset = offset_x, offset_y
+        self.effective_offset = offset_x//self.scale_factor.get(), offset_y//self.scale_factor.get()
 
     def update_image(self):
-        width = int(self.pil_image.width * self.scale_factor)
-        height = int(self.pil_image.height * self.scale_factor)
+        width = int(self.pil_image.width * self.scale_factor.get())
+        height = int(self.pil_image.height * self.scale_factor.get())
 
         self.resized_image = self.pil_image.resize((width, height), Image.Resampling.NEAREST)    # used nearest to avoid interpolation
         self.photo_image = ImageTk.PhotoImage(self.resized_image)
 
-        anchor_x, anchor_y = int(self.winfo_width()/2)+self.offset[0], int(self.winfo_height()/2)+self.offset[1]
+        anchor_x = int(self.winfo_width()/2)+self.effective_offset[0]*self.scale_factor.get()
+        anchor_y = int(self.winfo_height()/2)+self.effective_offset[1]*self.scale_factor.get()
+        if self.image_id:
+            self.delete(self.image_id)
         self.image_id = self.create_image(anchor_x, anchor_y, image=self.photo_image, anchor='center', tags= ('image'))
         if self.mask_id:
-            self.tkmask = ImageTk.PhotoImage(self.mask.resize((self.mask.width*self.scale_factor,self.mask.height*self.scale_factor),
-                                                              Image.Resampling.NEAREST))
+            self.resized_mask = self.mask.resize((self.mask.width*self.scale_factor.get(),self.mask.height*self.scale_factor.get()),
+                                                 Image.Resampling.NEAREST)
+            self.tkmask = ImageTk.PhotoImage(self.resized_mask)
             self.mask_id = self.create_image(anchor_x, anchor_y, image=self.tkmask, anchor= 'center', tags= ('image'))
 
     def zoom(self, event):
@@ -146,9 +185,9 @@ class ZoomDrag(tk.Canvas):
             return
 
         if event.delta > 0 or event.num == 4:
-            self.scale_factor += 1
+            self.scale_factor.set(self.scale_factor.get()+1)
         elif event.delta < 0 or event.num == 5:
-            self.scale_factor= max(self.scale_factor-1, 1)
+            self.scale_factor.set(max(self.scale_factor.get()-1, 1))
 
         self.update_image()
 
@@ -207,19 +246,55 @@ class ZoomDrag(tk.Canvas):
         draw = ImageDraw.Draw(self.mask)
         draw.polygon(item_coords, fill= self.mask_color)
 
-        if self.scale_factor != 1:
+        if self.scale_factor.get() != 1:
             self.mask = self.mask.resize((self.pil_image.width, self.pil_image.height), Image.Resampling.NEAREST)
-            self.tkmask = ImageTk.PhotoImage(self.mask.resize((self.mask.width*self.scale_factor,self.mask.height*self.scale_factor),
-                                                              Image.Resampling.NEAREST))
+            self.resized_mask = self.mask.resize((self.mask.width*self.scale_factor.get(),self.mask.height*self.scale_factor.get()),
+                                                Image.Resampling.NEAREST)
+            self.tkmask = ImageTk.PhotoImage(self.resized_mask)
         else:
-            self.tkmask = ImageTk.PhotoImage(self.mask)
+            self.resized_mask = self.mask
+            self.tkmask = ImageTk.PhotoImage(self.resized_mask)
         self.mask_id = self.create_image(bbox[0], bbox[1], image=self.tkmask, anchor=tk.NW, tags= ('image'))
 
-canvas = ZoomDrag(window)
-open_button = tk.Button(window, text="Open Image", command= open_hsi)
+    def pick_color(self, event)->str:
+        color = colorchooser.askcolor(title="Pick a Color", parent= root)
+        if color:
+            current_color.configure(bg= color[1])
+            self.graph_color = color[1]
+        return color[1]             # HEX color code
 
-open_button.grid(row= 0, pady= (5, 0))
-canvas.grid(row= 1, padx= 5, pady= 5)
+# define widgets
+canvas = ZoomDrag(root)
+menu_bar = tk.Menu(root)
+scale_monitor = tk.Frame(root, bg="gray80", bd=2, relief="solid", highlightbackground= 'black')
+scale_label = tk.Label(scale_monitor, textvariable= canvas.scale_factor, bg= 'gray80')
+color_monitor = tk.Frame(root)
+color_button = tk.Button(color_monitor, text= 'change\n(middle mouse)', command= lambda: canvas.pick_color(None))
+current_color = tk.Label(color_monitor, width= 1, height= 1, bg= canvas.graph_color)
+
+# Create a File menu
+file_menu = tk.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Change CWD", command= choose_cwd)
+file_menu.add_command(label="Open", command= open_hsi)
+file_menu.add_command(label="Save Canvas", command= save_image)
+
+# Add the File menu to the menu bar
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+# Configure the root window to use the menu bar
+root.config(menu=menu_bar)
+
+# place wigets
+scale_monitor.grid(row= 0, column= 0, padx= (5, 0))
+tk.Label(scale_monitor, text= 'scale: ', bg= 'gray80').grid(row= 0, column= 0, sticky= 'E')
+scale_label.grid(row= 0, column= 1, sticky= 'W')
+color_monitor.grid(row= 1, column= 0, padx= (5, 0))
+tk.Label(color_monitor, text= 'graph color').grid(row= 0, column= 0, sticky= 'S', columnspan= 2)
+color_button.grid(row= 1, column= 0, columnspan= 2)
+tk.Label(color_monitor, text= 'current:').grid(row= 2, column= 0, pady= 10)
+current_color.grid(row= 2, column= 1, sticky= 'we')
+
+canvas.grid(row= 0, column= 1, rowspan= 2, padx= 5, pady= 5)
 
 # Start the Tkinter event loop
-window.mainloop()
+root.mainloop()
